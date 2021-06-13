f9268d0f4ef91265aab1686c2d67b3c1952e423a
Fabrizio Ervini <faber.rvn@gmail.com>
2021-06-13 17:31:45 +0200

    feat: add posts section

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Posts.scss';
+
+const Posts = () => {
+  return (
+    <section className="posts">
+      <header className="posts__header">
+        <div className="posts__header__links">
+          <p className="posts__header__link active">Latest</p>
+          <p className="posts__header__link">Popular</p>
+        </div>
+        <label htmlFor="filter-post" className="posts__header__input">
+          <span className="posts__header__input-label">Show:</span>
+          <select id="filter-post" className="posts__header__input-select">
+            <option value="all">All</option>
+            <option value="latest">Latest</option>
+            <option value="popular">Popular</option>
+          </select>
+        </label>
+      </header>
+    </section>
+  );
+};
+
+export default Posts;
