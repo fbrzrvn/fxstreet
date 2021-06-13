48c4cb81cdab99c913450c019442cd0d2fc00ecd
Fabrizio Ervini <faber.rvn@gmail.com>
2021-06-13 17:28:14 +0200

    feat: add aside

---
+++ src/components/Aside/Aside.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Aside.scss';
+
+const Aside = () => {
+  return (
+    <aside className="aside">
+      <div className="aside__top">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed turpis
+          turpis, dictum sit amet lacus nec, maximus porta tortor.
+        </p>
+      </div>
+      <div className="aside__bottom">
+        <p>Nullam ut ipsum tincidunt, blandit quam at, imperdiet justo.</p>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
