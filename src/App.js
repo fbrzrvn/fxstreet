0d0c83b9e278a9f6398e6b40a29888fbf1349626
Fabrizio Ervini <faber.rvn@gmail.com>
2021-06-13 12:12:59 +0200

    feat: add react eslint and tools

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/index.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>FXStreet</h1>
+    </div>
+  );
+}
+
+export default App;
