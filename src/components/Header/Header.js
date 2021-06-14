68533bcf41024b7960edea5e05bd4689dade7ee3
Fabrizio Ervini <faber.rvn@gmail.com>
2021-06-14 17:25:03 +0200

    feat: add divider and search icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import { bool, func } from 'prop-types';
 import React from 'react';
-import { BsClock, BsList, BsPerson, BsX } from 'react-icons/bs';
+import { BsClock, BsList, BsPerson, BsSearch, BsX } from 'react-icons/bs';
 import MobileLogo from '../../assets/logo-mobile.jpeg';
 import Logo from '../../assets/logo-white.png';
 import { getCurrentTime } from '../../helpers';
@@ -11,44 +11,52 @@ const Header = ({ isOpen, toggleNavbar }) => {
   const currentWidth = useWindowSize();
 
   return (
-    <nav className="navbar">
-      <button type="button" className="navbar__mobile" onClick={toggleNavbar}>
-        {isOpen ? <BsX /> : <BsList />}
-      </button>
-      <img
-        src={currentWidth < 768 ? MobileLogo : Logo}
-        alt="logo"
-        className="navbar__logo"
-      />
-      <ul className="navbar__list">
-        <li className="navbar__item">
-          <div className="navbar__item-time">
-            <BsClock />
-            <span className="navbar__item-timestamp">{getCurrentTime()}</span>
-          </div>
-        </li>
-        {currentWidth < 576 ? (
+    <header className="header">
+      <nav className="navbar">
+        <button type="button" className="navbar__mobile" onClick={toggleNavbar}>
+          {isOpen ? <BsX /> : <BsList />}
+        </button>
+        <img
+          src={currentWidth < 768 ? MobileLogo : Logo}
+          alt="logo"
+          className="navbar__logo"
+        />
+        <ul className="navbar__list">
+          <div className="navbar__item__divider" />
           <li className="navbar__item">
-            <div className="navbar__item-user">
-              <BsPerson />
+            <BsSearch />
+          </li>
+          <div className="navbar__item__divider" />
+          <li className="navbar__item">
+            <div className="navbar__item-time">
+              <BsClock />
+              <span className="navbar__item-timestamp">{getCurrentTime()}</span>
             </div>
           </li>
-        ) : (
-          <>
+          <div className="navbar__item__divider" />
+          {currentWidth < 576 ? (
             <li className="navbar__item">
-              <button type="button" className="navbar__link">
-                Sign In
-              </button>
+              <div className="navbar__item-user">
+                <BsPerson />
+              </div>
             </li>
-            <li className="navbar__item">
-              <button type="button" className="navbar__button">
-                Sign Up
-              </button>
-            </li>
-          </>
-        )}
-      </ul>
-    </nav>
+          ) : (
+            <>
+              <li className="navbar__item">
+                <button type="button" className="navbar__link">
+                  Sign In
+                </button>
+              </li>
+              <li className="navbar__item">
+                <button type="button" className="navbar__button">
+                  Sign Up
+                </button>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
   );
 };
 
