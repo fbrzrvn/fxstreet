54c7780b380d4a96c028b66f8806f154575aab78
Fabrizio Ervini <faber.rvn@gmail.com>
2021-06-14 13:49:03 +0200

    feat: add fetched posts and fix styles

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '../../redux/Post/postActions';
+import { postSelector } from '../../redux/Post/postSelector';
 import Post from './Post';
 import './Posts.scss';
 
 const Posts = () => {
+  const dispatch = useDispatch();
+  const { posts } = useSelector(postSelector);
+  const [isPopular, setIsPopular] = useState(false);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  posts.sort((a, b) => {
+    return b.publicationTime.slice(8, 10) - a.publicationTime.slice(8, 10);
+  });
+
+  const currentCategory = isPopular
+    ? posts.filter((post) => post.isPopular === true)
+    : posts.filter((post) => post.isPopular !== true);
+
   return (
     <section className="posts">
       <header className="posts__header">
         <div className="posts__header__links">
-          <p className="posts__header__link active">Latest</p>
-          <p className="posts__header__link">Popular</p>
+          <button
+            type="button"
+            className={`posts__header__btn ${!isPopular && 'active'}`}
+            onClick={() => setIsPopular(!isPopular)}
+          >
+            Latest
+          </button>
+          <button
+            type="button"
+            className={`posts__header__btn ${isPopular && 'active'}`}
+            onClick={() => setIsPopular(!isPopular)}
+          >
+            Popular
+          </button>
         </div>
         <label htmlFor="filter-post" className="posts__header__input">
           <span className="posts__header__input-label">Show:</span>
@@ -19,7 +50,9 @@ const Posts = () => {
           </select>
         </label>
       </header>
-      <Post />
+      {currentCategory.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </section>
   );
 };
