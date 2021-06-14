affe468e8217a32f97c02572038d6fa733c55049
Fabrizio Ervini <faber.rvn@gmail.com>
2021-06-14 13:50:22 +0200

    feat: add post content update helpers and styles

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,3 +1,6 @@
+import { sanitize } from 'dompurify';
+import parse from 'html-react-parser';
+import { object } from 'prop-types';
 import React, { useState } from 'react';
 import { AiOutlineFileSearch } from 'react-icons/ai';
 import {
@@ -9,11 +12,16 @@ import {
   BsHeartFill,
   BsThreeDots,
 } from 'react-icons/bs';
+import { formatPostDate } from '../../../helpers';
 import './Post.scss';
 
-const Post = () => {
+const Post = ({ post }) => {
   const [isLiked, setIsLiked] = useState(false);
   const [isSaved, setIsSaved] = useState(false);
+
+  const cleanHTML = sanitize(post.content, {
+    USE_PROFILES: { html: true },
+  });
 
   return (
     <article className="article">
@@ -22,38 +30,44 @@ const Post = () => {
           <span className="article__header__feeds__icon">
             <AiOutlineFileSearch />
           </span>
-          <p className="article__header__feeds__feed">feed</p>
+          <p className="article__header__feeds__feed">{post.feed}</p>
           <span className="article__header__feeds__icon">
             <BsCaretRightFill />
           </span>
-          <p className="article__header__feeds__subfeed">subfeed</p>
+          <p className="article__header__feeds__subfeed">{post.subFeed}</p>
         </div>
         <div className="article__header__date">
           <BsClock />
-          <p>article date</p>
+          <p>{formatPostDate(post.publicationTime)}</p>
         </div>
       </header>
       <div className="article__title">
         <img
-          src="https://cdn2.iconfinder.com/data/icons/rounded-labels/154/round-label-sticker-copyright-author-512.png"
+          src={post.author.imageUrl}
           alt="article-author"
           className="article__title__avatar"
         />
         <div className="article__title__text">
-          <span>article author</span>
+          <span>{post.author.name}</span>
           <span>|</span>
-          <span>article author company</span>
-          <h4 className="article__title__text__title">article title</h4>
+          <span>{post.author.companyName}</span>
+          <h4 className="article__title__text__title">{post.title}</h4>
         </div>
       </div>
       <div className="article__content">
-        <p>article content</p>
-        <img src="" alt="article" />
+        <div className="article__content__text">{parse(cleanHTML)}</div>
+        {post.imageUrl && (
+          <img
+            src={post.imageUrl}
+            alt="article"
+            className="article__content__img"
+          />
+        )}
       </div>
       <footer className="article__footer">
         <button
           type="button"
-          className={`article__footer__btn ${isLiked && 'liked'}`}
+          className={`article__footer__btn ${isLiked ? 'liked' : 'like'}`}
           onClick={() => setIsLiked(!isLiked)}
         >
           {isLiked ? <BsHeartFill /> : <BsHeart />}
@@ -61,18 +75,22 @@ const Post = () => {
         </button>
         <button
           type="button"
-          className={`article__footer__btn ${isSaved && 'saved'}`}
+          className={`article__footer__btn ${isSaved ? 'saved' : 'save'}`}
           onClick={() => setIsSaved(!isSaved)}
         >
           {isSaved ? <BsBookmarkFill /> : <BsBookmark />}
           <span>{isSaved ? 'Saved!' : 'Save'}</span>
         </button>
-        <button type="button" className="article__footer__btn-action">
+        <button type="button" className="article__footer__btn">
           <BsThreeDots />
         </button>
       </footer>
     </article>
   );
+};
+
+Post.propTypes = {
+  post: object.isRequired,
 };
 
 export default Post;
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,3 +6,25 @@ export const getCurrentTime = () => {
   hours = hours >= 10 ? hours : `0${hours}`;
   return `${hours}:${minutes}`;
 };
+
+export const formatPostDate = (date) => {
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const monthIndex = date.slice(6, 7) - 1;
+  const month = months[monthIndex];
+  const day = date.slice(8, 10);
+  const time = date.slice(11, 16);
+  return `${month} ${day}, ${time}`;
+};
